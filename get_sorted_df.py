e18c7d33d90e0483e0dfa65de51c95fe14f25631
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-06-23 15:11:13 +0100

    numberbatch con grouped thresh sorted

--- get_sorted_df.py
+++ get_sorted_df.py
@@ -14,7 +14,7 @@ all_df = []
 
 print(f"concepts : {df.con.values[0:1000]}", flush=True)
 
-for c in df.con.values[0:3000]:
+for c in df.con.sample(n=3000, replace=False):
     con_df = df[df["con"] == c]
     con_df = con_df.sort_values("thresh", ascending=False)
 
