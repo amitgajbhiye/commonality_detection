512ba3acecea2dc1c387654b18e8dc8daab7515d
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-06-23 13:40:09 +0100

    numberbatch con grouped thresh sorted

--- get_sorted_df.py
+++ get_sorted_df.py
@@ -14,7 +14,7 @@ all_df = []
 
 print(f"concepts : {df.con.values[0:1000]}", flush=True)
 
-for c in df.con.values[0:2000]:
+for c in df.con.values[0:3000]:
     con_df = df[df["con"] == c]
     con_df = con_df.sort_values("thresh", ascending=False)
 
@@ -26,5 +26,5 @@ for c in df.con.values[0:2000]:
 
 final_df = pd.concat(all_df)
 
-sorted_out_file = "output_files/clustered_relbert_scored_file/con1000_grouped_thresh_sored_numberbatch_relation_probs.txt"
+sorted_out_file = "output_files/clustered_relbert_scored_file/con3000_grouped_thresh_sored_numberbatch_relation_probs.txt"
 final_df.to_csv(sorted_out_file, sep="\t", index=False)
